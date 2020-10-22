e2ff1c5b6b7cd8dc8b0cc921a3893c2b52428e73
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-10-22 15:10:30 +0500

    Add small changes in  Snacks page

--- src/pages/snacks.js
+++ src/pages/snacks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import SnacksShowcase from "../components/snacksShowcase/snacksShowcase";
 import Sample from "../components/sample/sample";
@@ -10,6 +11,10 @@ import Subscribe from "../components/subscribe/subscribe";
 const snacks = () => {
   return (
     <>
+      <Head>
+        <title>Robinhood Snacks</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <SnacksShowcase />
       <Sample />
       <Podcast />
