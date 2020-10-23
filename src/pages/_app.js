1307da5bc34aabbf6b05cc1437ba201c12be6c36
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-10-23 14:38:40 +0500

    Add About-us page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import Layout from "../components/layout/Layout";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <CssBaseline />
       <Component {...pageProps} />
     </Layout>
   );
