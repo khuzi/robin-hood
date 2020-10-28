91a10bd5e3e352cd4da6b327ee64687e91ae86a2
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-10-28 08:35:59 +0500

    samll chage in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,13 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <Layout>
       <CssBaseline />
