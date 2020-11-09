96aafb6b565685b9069311521cf465ec7cca8dc2
khuzaima <khuzaimayaseen@gmail.com>
2020-11-10 01:13:53 +0500

    some changes in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import Layout from "../components/layout/Layout";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "../styles/globals.css";
 
+import PropTypes from "prop-types";
+
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -17,5 +19,10 @@ function MyApp({ Component, pageProps }) {
     </Layout>
   );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default MyApp;
